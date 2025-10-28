5b2c9b2362bcf41b748a59d97cd9c47b13e4b066
Johnson Valenzuela <johnsondavisv4@hotmail.com>
2025-10-28 23:16:33 +0000

    refactor: Remove medicamento module and related DTOs, controller, and service

--- backend/src/medicamento/medicamento.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MedicamentoService } from './medicamento.service';
-import { MedicamentoController } from './medicamento.controller';
-
-@Module({
-  controllers: [MedicamentoController],
-  providers: [MedicamentoService],
-})
-export class MedicamentoModule {}
